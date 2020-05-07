07085f7c9fd1ab5d72805b9042f3729077b6faac
turbold <turbold_1007@yahoo.com>
2020-05-07 17:43:30 +0800

    state bolon search controller nemsen

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,31 @@
 require("@babel/polyfill");
 import Search from "./model/Search";
 
-let search = new Search('pasta');
-search.doSearch().then(r => console.log(r));
+//  Web App - ийн төлөв 
+// Хайлтын query, үр дүн 
+// тухайн үзүүлж байгаа жор
+// Лайкласан жорууд
+// Захиалж байгаа жорын найрлаганууд
+
+const state = {};
+
+const controlSearch = async () => {
+    console.log('Дарагдлаа');
+    // 1) web -Ээс хайлтын түлхүүр үгийг гаргаж авна.
+    const query = 'pizza';
+    if(query) {
+    // 2) шинээр хайлтын обьетыг үүсгэж өгнө.
+    state.search = new Search(query);
+    // 3) хайлт хийхэд зориулж дэлгэцийн UI ийг бэлдэнэ.
+    // 4) хайлтыг гүйцэтгэнэ.
+   await state.search.doSearch();
+    // 5) Хайлтын үр дүнг дэлгэцэнд үзүүлнэ. 
+    console.log(state.search.result);
+    } 
+
+}
+
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
